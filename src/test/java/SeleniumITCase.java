e1e516150ea1fa91d961fd0801fad998d2033a7e
David Banks <amoebae@gmail.com>
2017-11-10 17:27:16 +0000

    fixing to work with mvn verify

--- src/test/java/SeleniumITCase.java
+++ src/test/java/SeleniumITCase.java
@@ -1,20 +1,22 @@
 
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
-public class SeleniumTest {
+public class SeleniumITCase {
     @Test
-    public void timelineClickOpensRelevantSessionPaper() {
+    public void timelineClickOpensRelevantSessionPaper() throws Exception {
         WebDriver driver = new FirefoxDriver();
+        driver.manage().window().setPosition(new Point(0,0));
+        driver.manage().window().setSize(new Dimension(1024,768));
+
         driver.get("http://192.168.122.43/");
+
+
 
         WebElement element = driver.findElement(By.className("paper-pad"));
         List<WebElement> canvases = driver.findElements(By.cssSelector("div#div_g canvas"));
@@ -23,6 +25,7 @@ public class SeleniumTest {
         WebElement theCanvas = canvases.get(0);
 
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", theCanvas);
+//        Thread.sleep(500);
 
         Actions foo = new Actions(driver);
         foo.moveToElement(theCanvas).moveByOffset(10, 10).click().build().perform();
